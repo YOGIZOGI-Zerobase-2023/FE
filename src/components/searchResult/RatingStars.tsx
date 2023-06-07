fb92bc257ec6559f119dda24937ea030f1b6ca6d
cloudedpanther <rickeykoh@ajou.ac.kr>
2023-06-07 23:28:16 +0900

    ✨ feat: add RatingStars component
    - show five gray stars by default
    - fill stars up with yellow color from left according to the rate

---
+++ src/components/searchResult/RatingStars.tsx
@@ -0,0 +1,32 @@
+import { AiFillStar } from 'react-icons/ai';
+
+interface IRatingStars {
+  rate: number;
+}
+
+const RatingStars = ({ rate }: IRatingStars) => {
+  const starFill = (rate / 5) * 100;
+
+  return (
+    <div className="flex items-center gap-1">
+      <div className="relative">
+        <div className="flex">
+          {Array.from({ length: 5 }, () => null).map(() => (
+            <AiFillStar className="text-gray-300" />
+          ))}
+        </div>
+        <div
+          className="absolute left-0 top-0 z-10 flex overflow-hidden"
+          style={{ width: `${starFill}%` }}
+        >
+          {Array.from({ length: 5 }, () => null).map(() => (
+            <AiFillStar className="text-yellow-300 flex-none" />
+          ))}
+        </div>
+      </div>
+      <p className="text-sm leading-none">{rate}</p>
+    </div>
+  );
+};
+
+export default RatingStars;
