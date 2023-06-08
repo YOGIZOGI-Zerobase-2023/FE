c9b3e2d82f536b01c5ae873c226da9c896dcbb5c
cloudedpanther <rickeykoh@ajou.ac.kr>
2023-06-08 17:32:10 +0900

    🎨 style: change padding of layout

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -5,7 +5,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="relative">
       <Nav />
-      <main className="py-10">{children}</main>
+      <main className="pt-16 pb-20 md:pb-12 min-h-screen">{children}</main>
       <Footer />
     </div>
   );
